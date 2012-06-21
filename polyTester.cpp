637e0ae3fb026a42cd8d3a4577869438c2debdaf
Victoria Wu <victoria.chen.wu@gmail.com>
2012-06-20 21:06:26 -0500

    Initial Commit - polyTester

---
+++ polyTester.cpp
@@ -0,0 +1,18 @@
+/*
+CS 352, Summer '12
+Victoria Wu
+
+Polynomial ADT Tester
+-----------------------------------------
+
+*/
+
+#include <iostream>
+
+int main()
+{
+	Poly pol1;
+	
+	
+	return 0;
+}
