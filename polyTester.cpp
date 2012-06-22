fd6b3d385547629d5fb356e8d9bff5aca7260066
Victoria Wu <victoria.chen.wu@gmail.com>
2012-06-22 00:09:30 -0500

    Started testing of term, addTerm

--- polyTester.cpp
+++ polyTester.cpp
@@ -8,10 +8,18 @@ Polynomial ADT Tester
 */
 
 #include <iostream>
+#include "poly.h"
 
 int main()
 {
+
 	Poly pol1;
+	int coeff, tempExp;
+	cin >> coeff ;
+	cin >> tempExp;
+	
+	cout << "Coeff: " << coeff <<endl << "Exp: " << tempExp << endl;
+	//pol1.addTerm(5, 
 	
 	
 	return 0;
